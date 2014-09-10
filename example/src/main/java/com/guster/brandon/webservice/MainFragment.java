0f4521d26f9f79c0808f3cbf007ef834959adb27
Gusterwoei <gusterwoei@goingenious.com>
2014-09-10 16:43:45 +0800

    Added something

--- example/src/main/java/com/guster/brandon/webservice/MainFragment.java
+++ example/src/main/java/com/guster/brandon/webservice/MainFragment.java
@@ -1,11 +1,9 @@
 package com.guster.brandon.webservice;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
